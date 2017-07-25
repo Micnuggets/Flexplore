a6a3aa52e83baaa91d2f427af0dff41f6fd4abbe
Mic <michellelau@live.nl>
2017-07-25 22:59:13 +0200

    The commit before first time putting online

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,5 @@
 class Location < ApplicationRecord
+  def self.search(search)
+    where("name ILIKE ? OR ingredients ILIKE ? OR cooking_instructions ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+  end
 end
