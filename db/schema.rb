5663e199d75f641a3f990228788ed55fc6babaa5
Mic <michellelau@live.nl>
2017-07-27 18:13:40 +0200

    With version of 26 July. after all those errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726075723) do
+ActiveRecord::Schema.define(version: 20170727134644) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20170726075723) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.float "pricemonth"
+    t.string "image"
   end
 
 end
